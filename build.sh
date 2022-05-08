4fe37b8c763cef23c01b30461781779307228621
Sean Macdonald <sean.macdonald@rmp.uhn.ca>
2022-05-08 18:04:33 -0400

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#	create the certificates. they will be embedded in the binary
+#	create the certificates. They will be embedded in the binary
+#	should create localhost.pem and locahost-key.pem
 #	@see: https://github.com/FiloSottile/mkcert#readme
 
 #	mkcert -ecdsa localhost
