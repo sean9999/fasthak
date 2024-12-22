461c5827a2c704f2ee3fd37155c885d1bd6cce85
Sean Macdonald <Sean.macdonald@hardrockdigital.com>
2024-12-22 14:32:22 -0500

    re-smudge

--- certs/clean.sh
+++ certs/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SALT_FIXED=FEEF
+SALT_FIXED=FEEF60061
 PASS_FIXED=foobarbat
 
-openssl enc -base64 -aes-256-ecb -S $SALT_FIXED -k $PASS_FIXED
+openssl enc -base64 -iter -S $SALT_FIXED -k $PASS_FIXED
--- certs/smudge.sh
+++ certs/smudge.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # No salt is needed for decryption.
-PASS_FIXED=<your-passphrase>
+PASS_FIXED=foobarbat
 
 # If decryption fails, use `cat` instead. 
 # Error messages are redirected to /dev/null.
-openssl enc -d -base64 -aes-256-ecb -k $PASS_FIXED 2> /dev/null || cat
+openssl enc -d -base64 -iter -k $PASS_FIXED 2> /dev/null || cat
